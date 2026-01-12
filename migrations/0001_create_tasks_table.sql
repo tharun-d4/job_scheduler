a3f04f7d4555e6b23b8e38bceb7ea444c33e0bf4
Tharun <tharun.damera4@gmail.com>
2026-01-12 23:11:44 +0530

    Add create tasks table migration and run it using sqlx cli

---
+++ migrations/0001_create_tasks_table.sql
@@ -0,0 +1,19 @@
+-- Add migration script here
+
+CREATE TYPE task_status AS ENUM ('pending', 'running', 'completed', 'failed');
+
+CREATE TABLE tasks (
+  id UUID PRIMARY KEY,
+  task_type VARCHAR(100) NOT NULL,
+  payload JSONB NOT NULL,
+  status task_status NOT NULL,
+  priority SMALLINT NOT NULL,
+  max_retries SMALLINT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  started_at TIMESTAMPTZ,
+  completed_at TIMESTAMPTZ,
+  worker_id UUID,
+  attempts SMALLINT,
+  error_message TEXT,
+  result JSONB
+);
