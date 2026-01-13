2d2359de39b7b334ff845328a8f3ad77a43bbb62
Tharun <tharun.damera4@gmail.com>
2026-01-13 17:04:37 +0530

    Add the result enum to the main fn return type and propogate db errors

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -1,18 +1,23 @@
-use api_server::{app, db::connection, state};
+use api_server::{app, db::connection, error, state};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), error::ServerError> {
     dotenvy::dotenv().ok();
 
-    let pool = connection::create_pool().await.unwrap();
-    connection::run_migrations(&pool).await.unwrap();
+    let pool = connection::create_pool().await?;
+    connection::run_migrations(&pool).await?;
 
     let state = state::AppState::new(pool);
     let app = app::create_router(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .unwrap();
+    let host = std::env::var("SERVER_HOST").expect("SERVER_HOST env variable not found");
+    let port = std::env::var("SERVER_PORT").expect("SERVER_PORT env variable not found");
+
+    let bind = format!("{}:{}", host, port);
+    println!("[+] Server running on {bind:?}...");
+
+    let listener = tokio::net::TcpListener::bind(bind).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
