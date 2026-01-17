557aa33b3b63a3fc34ecf4d375b68d669c31c2f0
Tharun <tharun.damera4@gmail.com>
2026-01-17 21:55:53 +0530

    Move the insert job tests from server to shared

--- server/tests/db_tests.rs
+++ server/tests/db_tests.rs
@@ -1,23 +1,23 @@
-use chrono::Utc;
-use sqlx::{PgPool, types::JsonValue};
-
-use server::db::{models::NewJob, queries};
-use shared::db::models::JobStatus;
-
-#[sqlx::test(migrations = "../migrations")]
-async fn test_insert_job_returns_job_id(pool: PgPool) -> Result<(), sqlx::Error> {
-    let job_id = queries::insert_job(
-        &pool,
-        NewJob {
-            job_type: "new_job".to_string(),
-            payload: JsonValue::String("A new job".to_string()),
-            status: JobStatus::Pending,
-            priority: 1,
-            max_retries: 5,
-            created_at: Utc::now(),
-        },
-    )
-    .await?;
-    println!("job_id: {:?}", job_id);
-    Ok(())
-}
+//use chrono::Utc;
+//use sqlx::{PgPool, types::JsonValue};
+//
+//use server::db::{models::NewJob, queries};
+//use shared::db::models::JobStatus;
+//
+//#[sqlx::test(migrations = "../migrations")]
+//async fn test_insert_job_returns_job_id(pool: PgPool) -> Result<(), sqlx::Error> {
+//    let job_id = queries::insert_job(
+//        &pool,
+//        NewJob {
+//            job_type: "new_job".to_string(),
+//            payload: JsonValue::String("A new job".to_string()),
+//            status: JobStatus::Pending,
+//            priority: 1,
+//            max_retries: 5,
+//            created_at: Utc::now(),
+//        },
+//    )
+//    .await?;
+//    println!("job_id: {:?}", job_id);
+//    Ok(())
+//}
---
+++ shared/tests/db_query_tests.rs
@@ -0,0 +1,25 @@
+use chrono::Utc;
+use sqlx::{PgPool, types::JsonValue};
+
+use shared::db::{
+    models::{JobStatus, NewJob},
+    queries,
+};
+
+#[sqlx::test(migrations = "../migrations")]
+async fn test_insert_job_returns_job_id(pool: PgPool) -> Result<(), sqlx::Error> {
+    let job_id = queries::insert_job(
+        &pool,
+        NewJob {
+            job_type: "new_job".to_string(),
+            payload: JsonValue::String("A new job".to_string()),
+            status: JobStatus::Pending,
+            priority: 1,
+            max_retries: 5,
+            created_at: Utc::now(),
+        },
+    )
+    .await?;
+    println!("job_id: {:?}", job_id);
+    Ok(())
+}
