429b9f4efee9f02bb9ff61a2a602b63868b0b05b
Tharun <tharun.damera4@gmail.com>
2026-01-26 21:03:41 +0530

    Use that sample test fixture in the api test of server

--- server/tests/api_tests.rs
+++ server/tests/api_tests.rs
@@ -1,7 +1,7 @@
 use axum::http::StatusCode;
 use sqlx::PgPool;
+use uuid::Uuid;
 
-use server::handlers::JobId;
 use shared::db::models::Job;
 
 mod test_server;
@@ -30,26 +30,15 @@ async fn test_create_job(pool: PgPool) {
     response.assert_status(StatusCode::CREATED);
 }
 
-#[sqlx::test(migrations = "../migrations")]
+#[sqlx::test(
+    migrations = "../migrations",
+    fixtures(path = "../../test_fixtures", scripts("jobs"))
+)]
 async fn test_get_job(pool: PgPool) {
     let server = test_server::build_test_server(pool);
-    let create_job_response = server
-        .post("/jobs")
-        .json(&serde_json::json!({
-            "job_type": "send_email",
-            "payload": {
-                "to": "to_email@mail.com",
-                "from": "job_scheduler@mail.com",
-                "subject": "This is a sample test",
-                "body": "Yes this is just a sample api test"
-            },
-            "priority": 10,
-            "max_retries": 5,
-        }))
-        .await;
-
-    create_job_response.assert_status(StatusCode::CREATED);
-    let job_id = create_job_response.json::<JobId>().job_id;
+    let job_id = "019bfadc-28bb-781d-9d22-acf23fe50117"
+        .parse::<Uuid>()
+        .unwrap();
 
     let get_job_response = server.get(&format!("/jobs/{}", job_id)).await;
     get_job_response.assert_status_ok();
