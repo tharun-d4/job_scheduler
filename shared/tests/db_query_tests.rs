c3ec00f9d1bfb0a989794aa60b222ef5911d2550
Tharun <tharun.damera4@gmail.com>
2026-01-26 21:10:01 +0530

    Use the same test fixture for the db test as well

--- shared/tests/db_query_tests.rs
+++ shared/tests/db_query_tests.rs
@@ -1,5 +1,6 @@
 use chrono::Utc;
 use sqlx::{PgPool, types::JsonValue};
+use uuid::Uuid;
 
 use shared::db::{
     models::{JobStatus, NewJob},
@@ -23,20 +24,14 @@ async fn test_insert_job_returns_job_id(pool: PgPool) -> Result<(), sqlx::Error>
     Ok(())
 }
 
-#[sqlx::test(migrations = "../migrations")]
+#[sqlx::test(
+    migrations = "../migrations",
+    fixtures(path = "../../test_fixtures", scripts("jobs"))
+)]
 async fn test_get_job_by_id(pool: PgPool) -> Result<(), sqlx::Error> {
-    let job_id = queries::insert_job(
-        &pool,
-        NewJob {
-            job_type: "new_job".to_string(),
-            payload: JsonValue::String("A new job".to_string()),
-            status: JobStatus::Pending,
-            priority: 1,
-            max_retries: 5,
-            created_at: Utc::now(),
-        },
-    )
-    .await?;
+    let job_id = "019bfadc-28bb-781d-9d22-acf23fe50117"
+        .parse::<Uuid>()
+        .unwrap();
     let job = queries::get_job_by_id(&pool, job_id).await;
 
     assert!(job.is_some());
