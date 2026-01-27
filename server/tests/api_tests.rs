40f2dfc3b5d66e10deef7e3c58be220946049a79
Tharun <tharun.damera4@gmail.com>
2026-01-27 12:41:43 +0530

    Add proper success and failure tests for create_job and get_job apis in server

--- server/tests/api_tests.rs
+++ server/tests/api_tests.rs
@@ -6,13 +6,10 @@ use shared::db::models::Job;
 
 mod test_server;
 
-fn _sleep(secs: u64) {
-    std::thread::sleep(std::time::Duration::from_secs(secs));
-}
-
 #[sqlx::test(migrations = "../migrations")]
-async fn test_create_job(pool: PgPool) {
+async fn create_job_with_valid_payload_returns_201(pool: PgPool) {
     let server = test_server::build_test_server(pool);
+
     let response = server
         .post("/jobs")
         .json(&serde_json::json!({
@@ -30,12 +27,34 @@ async fn test_create_job(pool: PgPool) {
     response.assert_status(StatusCode::CREATED);
 }
 
+#[sqlx::test(migrations = "../migrations")]
+async fn create_job_with_invalid_payload_returns_422(pool: PgPool) {
+    let server = test_server::build_test_server(pool);
+
+    // job_type and max_retries are missing in the request payload
+    // the response status code should 422
+    let response = server
+        .post("/jobs")
+        .json(&serde_json::json!({
+            "payload": {
+                "to": "to_email@mail.com",
+                "from": "job_scheduler@mail.com",
+                "subject": "This is a sample test",
+                "body": "Yes this is just a sample api test"
+            },
+            "priority": 10,
+        }))
+        .await;
+    response.assert_status(StatusCode::UNPROCESSABLE_ENTITY);
+}
+
 #[sqlx::test(
     migrations = "../migrations",
     fixtures(path = "../../test_fixtures", scripts("jobs"))
 )]
-async fn test_get_job(pool: PgPool) {
+async fn get_job_for_valid_job_id_returns_job(pool: PgPool) {
     let server = test_server::build_test_server(pool);
+
     let job_id = "019bfadc-28bb-781d-9d22-acf23fe50117"
         .parse::<Uuid>()
         .unwrap();
@@ -45,4 +64,21 @@ async fn test_get_job(pool: PgPool) {
 
     let job = get_job_response.json::<Job>();
     assert_eq!(job_id, job.id);
+}
+
+#[sqlx::test(
+    migrations = "../migrations",
+    fixtures(path = "../../test_fixtures", scripts("jobs"))
+)]
+async fn get_job_for_invalid_job_id_returns_404(pool: PgPool) {
+    let server = test_server::build_test_server(pool);
+
+    // job_id is invalid as it is not present in DB
+    // returns a 404 status code: Job not found error
+    let job_id = "019bfadc-28bb-781d-9d22-acf23fe50116"
+        .parse::<Uuid>()
+        .unwrap();
+
+    let get_job_response = server.get(&format!("/jobs/{}", job_id)).await;
+    get_job_response.assert_status(StatusCode::NOT_FOUND);
 }
