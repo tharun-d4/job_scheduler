7d208257e1d91ca1195a5993b4205ad303d898eb
Tharun <tharun.damera4@gmail.com>
2026-02-05 10:47:35 +0530

    Add AddressError and SmtpError to the WorkerError for error propagation

--- worker/src/error.rs
+++ worker/src/error.rs
@@ -1,12 +1,27 @@
+use lettre::{address::AddressError, transport::smtp::Error as SmtpError};
 use sqlx::Error as SqlxError;
 
 #[derive(Debug)]
 pub enum WorkerError {
     Database(SqlxError),
+    ParseEmail(AddressError),
+    SmtpError(SmtpError),
 }
 
 impl From<SqlxError> for WorkerError {
     fn from(err: SqlxError) -> Self {
         WorkerError::Database(err)
+    }
+}
+
+impl From<AddressError> for WorkerError {
+    fn from(err: AddressError) -> Self {
+        WorkerError::ParseEmail(err)
+    }
+}
+
+impl From<SmtpError> for WorkerError {
+    fn from(err: SmtpError) -> Self {
+        WorkerError::SmtpError(err)
     }
 }
