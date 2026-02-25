32d9e4df86bcdaebb943b05232016c5dc632bebb
Tharun <tharun.damera4@gmail.com>
2026-02-25 11:13:48 +0530

    Get the rows affected value when running any query to log it later

--- worker/src/db/queries.rs
+++ worker/src/db/queries.rs
@@ -4,24 +4,28 @@ use uuid::Uuid;
 
 use shared::db::models::{Job, JobStatus};
 
-pub async fn register(pool: &PgPool, worker_id: Uuid, pid: i32) -> Result<(), sqlx::Error> {
-    query("INSERT INTO workers (id, pid, started_at, last_heartbeat) VALUES ($1, $2, $3, $3);")
-        .bind(worker_id)
-        .bind(pid)
-        .bind(Utc::now())
-        .execute(pool)
-        .await?;
-    Ok(())
+pub async fn register(pool: &PgPool, worker_id: Uuid, pid: i32) -> Result<u64, sqlx::Error> {
+    let inserted_rows =
+        query("INSERT INTO workers (id, pid, started_at, last_heartbeat) VALUES ($1, $2, $3, $3);")
+            .bind(worker_id)
+            .bind(pid)
+            .bind(Utc::now())
+            .execute(pool)
+            .await?
+            .rows_affected();
+
+    Ok(inserted_rows)
 }
 
-pub async fn update_heartbeat(pool: &PgPool, worker_id: Uuid) -> Result<(), sqlx::Error> {
-    query("UPDATE workers SET last_heartbeat=$2 WHERE id=$1;")
+pub async fn update_heartbeat(pool: &PgPool, worker_id: Uuid) -> Result<u64, sqlx::Error> {
+    let updated_rows = query("UPDATE workers SET last_heartbeat=$2 WHERE id=$1;")
         .bind(worker_id)
         .bind(Utc::now())
         .execute(pool)
-        .await?;
+        .await?
+        .rows_affected();
 
-    Ok(())
+    Ok(updated_rows)
 }
 
 pub async fn claim_job(
@@ -61,8 +65,8 @@ pub async fn mark_job_as_completed(
     job_id: Uuid,
     worker_id: Uuid,
     result: Option<JsonValue>,
-) -> Result<(), sqlx::Error> {
-    query(
+) -> Result<u64, sqlx::Error> {
+    let updated_rows = query(
         "UPDATE jobs
         SET
             status = $1,
@@ -79,9 +83,10 @@ pub async fn mark_job_as_completed(
     .bind(worker_id)
     .bind(JobStatus::Running)
     .execute(pool)
-    .await?;
+    .await?
+    .rows_affected();
 
-    Ok(())
+    Ok(updated_rows)
 }
 
 pub async fn store_job_error(
@@ -90,8 +95,8 @@ pub async fn store_job_error(
     worker_id: Uuid,
     error: String,
     backoff_secs: i16,
-) -> Result<(), sqlx::Error> {
-    query(
+) -> Result<u64, sqlx::Error> {
+    let updated_rows = query(
         "UPDATE jobs
         SET
             status = $1,
@@ -106,23 +111,25 @@ pub async fn store_job_error(
     .bind(job_id)
     .bind(worker_id)
     .execute(pool)
-    .await?;
+    .await?
+    .rows_affected();
 
-    Ok(())
+    Ok(updated_rows)
 }
 
 pub async fn update_worker_shutdown_time(
     pool: &PgPool,
     worker_id: Uuid,
-) -> Result<(), sqlx::Error> {
-    query(
+) -> Result<u64, sqlx::Error> {
+    let updated_rows = query(
         "UPDATE workers
         SET shutdown_at = NOW()
         WHERE id = $1;",
     )
     .bind(worker_id)
     .execute(pool)
-    .await?;
+    .await?
+    .rows_affected();
 
-    Ok(())
+    Ok(updated_rows)
 }
