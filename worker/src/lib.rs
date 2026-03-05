3420abb3368d33bd97b2b012a1dc246f504c7e29
Tharun <tharun.damera4@gmail.com>
2026-03-05 16:14:58 +0530

    Move the entire worker main fn code to init fn in lib and use that in main

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -3,3 +3,71 @@ pub mod error;
 pub mod executor;
 pub mod handlers;
 pub mod heartbeat;
+
+use tokio::signal::unix::{SignalKind, signal};
+use tracing::{error, info, instrument};
+use uuid::Uuid;
+
+use crate::{db::queries, error::WorkerError, handlers::email};
+use shared::{config::load_worker_config, db::connection, tracing::init_tracing};
+
+#[instrument]
+pub async fn init() -> Result<(), WorkerError> {
+    let _trace_guard = init_tracing("worker");
+    let config = load_worker_config("./config").expect("Config Error");
+
+    let pool = connection::create_pool(&config.database)
+        .await
+        .map_err(|e| WorkerError::permanent("Failed to establish db connection").set_source(e))?;
+    connection::run_migrations(&pool)
+        .await
+        .map_err(|e| WorkerError::permanent("Failed to run db migrations").set_source(e))?;
+
+    let worker_id = Uuid::now_v7();
+    let pid = std::process::id();
+
+    queries::register(&pool, worker_id, pid as i32).await?;
+    info!(
+        "Worker (ID: {:?}, PID: {}) has started running & registered itself",
+        worker_id, pid
+    );
+
+    heartbeat::start_heartbeat_task(pool.clone(), worker_id, config.worker.heartbeat).await;
+
+    let smtp_sender = email::smtp_sender(&config.mail_server.host, config.mail_server.port);
+    let client = reqwest::Client::new();
+
+    let mut terminate_signal = signal(SignalKind::terminate())
+        .map_err(|e| WorkerError::permanent("Failed to create a SIGTERM listener").set_source(e))?;
+    let mut iterrupt_signal = signal(SignalKind::interrupt())
+        .map_err(|e| WorkerError::permanent("Failed to create a SIGINT listener").set_source(e))?;
+
+    loop {
+        tokio::select! {
+            _ = terminate_signal.recv() => {
+                info!("Received Terminate Signal(SIGTERM)");
+                break;
+            }
+            _ = iterrupt_signal.recv() => {
+                info!("Received Interrupt Signal(SIGINT)");
+                break;
+            }
+            claim_result = queries::claim_job(&pool, worker_id, config.worker.lease_duration) => {
+                match claim_result {
+                    Ok(Some(job)) => {
+                        executor::execute_job(&pool, job, worker_id, smtp_sender.clone(), client.clone()).await?;
+                    }
+                    Ok(None) => {
+                        // No job to run
+                        tokio::time::sleep(std::time::Duration::from_millis(250)).await;
+                    }
+                    Err(err) => error!(error = ?err, "Claim job error"),
+                }
+            }
+        }
+    }
+    info!("Worker (ID: {:?}, PID: {}) shutting down", worker_id, pid);
+    queries::update_worker_shutdown_time(&pool, worker_id).await?;
+
+    Ok(())
+}
