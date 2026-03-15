35e5efd90616cb34a3ea9faea711c6664151628d
Tharun <tharun.damera4@gmail.com>
2026-03-15 18:51:11 +0530

    Use reqwest client to load test or benchmark the job submission api

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,6 +1,9 @@
 use std::process::Command;
 
-fn main() {
+use tokio::time::Instant;
+
+#[tokio::main]
+async fn main() {
     println!("Begin benchmarking...");
 
     let build_server = Command::new("cargo")
@@ -13,7 +16,49 @@ fn main() {
     let server_process = Command::new("./target/debug/server")
         .spawn()
         .expect("failed to spawn a server process");
-    println!("server_process pid: {:?}", server_process.id());
+    println!("Started Server (pid: {:?})", server_process.id());
+
+    let req_client = reqwest::Client::new();
+
+    const TOTAL_JOBS: u32 = 1000;
+    let start = Instant::now();
+    let mut success = 0;
+    let mut error = 0;
+    for _ in 0..TOTAL_JOBS {
+        let response = req_client
+            .post("http://127.0.0.1:8000/jobs")
+            .json(&serde_json::json!({
+                "job_type": "send_email",
+                "payload": {
+                    "to": "to_email@mail.com",
+                    "from": "job_scheduler@mail.com",
+                    "subject": "This is a sample load test / benchmark",
+                    "body": "Yes this is just a sample load test / benchmark"
+                },
+                "priority": 10,
+                "max_retries": 5,
+            }))
+            .send()
+            .await;
+        if let Ok(resp) = response
+            && resp.status() == reqwest::StatusCode::CREATED
+        {
+            success += 1;
+        } else {
+            error += 1;
+        }
+    }
+    println!("Submitted {} email jobs", TOTAL_JOBS);
+
+    let end = start.elapsed();
+    println!("Submission results: ");
+    println!("Duration: {:.2}sec", end.as_secs_f64());
+    println!("Successful: {success}");
+    println!("Errors: {error}");
+    println!(
+        "Rate: {:.2} jobs/sec",
+        TOTAL_JOBS as f64 / end.as_secs_f64()
+    );
 
     // kill the server process by sending SIGTERM
     Command::new("kill")
