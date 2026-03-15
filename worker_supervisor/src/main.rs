75cb60419578240f5e29197f5991f6380db615aa
Tharun <tharun.damera4@gmail.com>
2026-03-15 22:03:51 +0530

    Send SIGTERM to worker processes and wait for them to shutdown gracefully when supervisor is terminated

--- worker_supervisor/src/main.rs
+++ worker_supervisor/src/main.rs
@@ -1,4 +1,15 @@
-use std::process::{Child, Command};
+use std::{
+    process::{Child, Command},
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
+
+use nix::{
+    sys::signal::{self, Signal},
+    unistd::Pid,
+};
 
 use shared::config::load_supervisor_config;
 
@@ -10,7 +21,14 @@ fn main() {
         workers.push(spawn_worker());
     }
 
-    loop {
+    let process_terminated = Arc::new(AtomicBool::new(false));
+
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, process_terminated.clone())
+        .expect("Cannot register a SIGTERM signal handler");
+    signal_hook::flag::register(signal_hook::consts::SIGINT, process_terminated.clone())
+        .expect("Cannot register a SIGINT signal handler");
+
+    while !process_terminated.load(Ordering::Relaxed) {
         for worker in workers.iter_mut() {
             if let Ok(Some(status)) = worker.try_wait() {
                 println!("Worker exited with status: {status}");
@@ -20,12 +38,23 @@ fn main() {
         }
         std::thread::sleep(std::time::Duration::from_secs(config.poll_interval as u64));
     }
+
+    for mut child in workers {
+        match signal::kill(Pid::from_raw(child.id() as i32), Signal::SIGTERM) {
+            Ok(_) => println!("Sent SIGTERM to Worker (PID: {:?})", child.id()),
+            Err(e) => eprintln!("error: {:?}", e),
+        }
+        match child.wait() {
+            Ok(_) => println!("Worker killed (PID: {:?})", child.id()),
+            Err(e) => eprintln!("error: {:?}", e),
+        }
+    }
 }
 
 fn spawn_worker() -> Child {
     let child = Command::new("./target/debug/worker")
         .spawn()
         .expect("Failed to spawn worker process");
-    println!("Spawned Worker PID: {:?}", child.id());
+    println!("Spawned Worker (PID: {:?})", child.id());
     child
 }
