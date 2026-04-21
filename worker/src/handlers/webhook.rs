b1f14df8c7b0bdd89b442ed3216a585159441653
Tharun <tharun.damera4@gmail.com>
2026-04-21 19:56:16 +0530

    Categorize and handle all the webhook job type errors in the worker into permanent and temporary errors

--- worker/src/handlers/webhook.rs
+++ worker/src/handlers/webhook.rs
@@ -21,20 +21,50 @@ pub async fn send_webhook(
         _ => return Err(WorkerError::permanent("Invalid method")),
     };
 
-    let response = request
+    let api_result = request
         .json(&body)
         .timeout(std::time::Duration::from_secs(30))
         .send()
-        .await
-        .map_err(|e| WorkerError::permanent("Failed to send webhook request").set_source(e))?;
+        .await;
 
-    info!("response: {:?}", response);
+    match api_result {
+        Ok(response) => {
+            info!("response: {:?}", response);
+            if response.status().is_success() {
+                let response_json = response.json::<JsonValue>().await.map_err(|e| {
+                    WorkerError::permanent("Failed to deserialize webhook response").set_source(e)
+                })?;
+                info!("response_json: {:?}", response_json);
 
-    let response_json = response.json::<JsonValue>().await.map_err(|e| {
-        WorkerError::permanent("Failed to deserialize webhook response").set_source(e)
-    })?;
+                Ok(Some(response_json))
+            } else {
+                let status = response.status();
+                if status == 429 || status.is_server_error() {
+                    Err(WorkerError::temporary(&format!(
+                        "Got a temporary failure status: {:?}",
+                        status
+                    )))
+                } else {
+                    Err(WorkerError::permanent(&format!(
+                        "Got a permanent failure status: {:?}",
+                        status
+                    )))
+                }
+            }
+        }
+        Err(e) if e.is_connect() => Err(WorkerError::temporary("Connection error").set_source(e)),
 
-    info!("response_json: {:?}", response_json);
+        Err(e) if e.is_timeout() => {
+            Err(WorkerError::temporary("Request got timed out").set_source(e))
+        }
+        Err(e) if e.is_decode() => {
+            Err(WorkerError::permanent("Failed to deserialize webhook response").set_source(e))
+        }
 
-    Ok(Some(response_json))
+        Err(e) if e.is_redirect() => {
+            Err(WorkerError::permanent("Redirected so misconfigured url").set_source(e))
+        }
+
+        Err(e) => Err(WorkerError::temporary("Unknown error").set_source(e)),
+    }
 }
